13827a19d68613d31f38560be84e2227c0c24cd4
daadestroyer <nigamshubham2000@gmail.com>
2025-04-08 15:12:44 +0530

    added change 1

--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ import java.sql.Date;
 
 public class Main{
     public static void main(String[] args) {
+        System.out.println("-----------------------------");
         System.out.println("This is java program");
         System.out.println("Output from java program");
-        System.out.println("Current Date : "+new Date(2025, 4, 8));
+        System.out.println("Current Date :  "+new Date(2025, 4, 8));
+        System.out.println("-----------------------------");
     }
 }
