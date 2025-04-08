ee6c3e4d23a7552c0582a89d5c559e08a7c43348
daadestroyer <nigamshubham2000@gmail.com>
2025-04-08 15:07:09 +0530

    code added...

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.sql.Date;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("This is java program");
+        System.out.println("Output from java program");
+        System.out.println("Current Date : "+new Date(2025, 4, 8));
+    }
+}
