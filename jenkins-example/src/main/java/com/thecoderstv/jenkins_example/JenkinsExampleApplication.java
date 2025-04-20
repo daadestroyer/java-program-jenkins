1fd68fd275a854a32b5ede638fa76a6557d939a0
daadestroyer <nigamshubham2000@gmail.com>
2025-04-20 13:30:28 +0530

    code added...

---
+++ jenkins-example/src/main/java/com/thecoderstv/jenkins_example/JenkinsExampleApplication.java
@@ -0,0 +1,25 @@
+package com.thecoderstv.jenkins_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsExampleApplication.class, args);
+		System.out.println("-----------------------------");
+		System.out.println("This is java program");
+		System.out.println("Output from java program");
+		System.out.println("Current Date :  "+new Date(2025, 4, 8));
+		System.out.println("This is change 2");
+		System.out.println("This is change 3");
+
+		System.out.println("20 Apr 2025");
+		System.out.println("20 Apr 2025");
+		System.out.println(10/0);
+		System.out.println(10/0);
+		System.out.println("-----------------------------");
+	}
+
+}
---
+++ jenkins-example/src/test/java/com/thecoderstv/jenkins_example/JenkinsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecoderstv.jenkins_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
